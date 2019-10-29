6096d75b08032da1661c35eb5f42a6e893bbbd96
Alper-Enes-DURU <enesduru@gmail.com>
2019-10-29 01:18:46 +0000

    Finished fibonacci challenge

---
+++ fibonacci.rb
@@ -0,0 +1,30 @@
+def iterative_fib(num)
+  i = 0
+  a = 0
+  b = 1
+
+  if num == 1 || num == 0
+    return num
+  else
+    while i < num
+      a = b
+      b = a + b
+      i += 1
+    end
+    return b
+  end
+end
+
+def recursive_fib(num)
+  if num == 1 || num == 0
+    return num
+  end
+  return recursive_fib(num-1) + recursive_fib(num-2)
+end
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
